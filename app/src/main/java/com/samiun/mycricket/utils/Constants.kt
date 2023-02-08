e07b9cc1dbf914ba3e598aef959fdb497f4c8933
srsizan <samiunsizan@gmail.com>
2023-02-08 22:40:25 +0600

    NetWork Started

--- app/src/main/java/com/samiun/mycricket/utils/Constants.kt
+++ app/src/main/java/com/samiun/mycricket/utils/Constants.kt
@@ -3,5 +3,10 @@ package com.samiun.mycricket.utils
 class Constants {
     companion object{
         const val databaseName = "cricket_database"
+        //API's
+        const val apikey = ""
+        const val BASE_URL = ""
+        const val COUNTRY_END_POINT = ""
+
     }
 }
