52c78e14be1b2226bdfcbf690c534e52d7174853
srsizan <samiunsizan@gmail.com>
2023-02-10 22:26:42 +0600

    NetWork Started

--- app/src/main/java/com/samiun/mycricket/utils/Constants.kt
+++ app/src/main/java/com/samiun/mycricket/utils/Constants.kt
@@ -1,5 +1,8 @@
 package com.samiun.mycricket.utils
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 class Constants {
     companion object{
         const val databaseName = "cricket_database"
@@ -12,8 +15,25 @@ class Constants {
         const val LEAGUES_END_POINT ="leagues?$api_token"
         const val FIXTURE_END_POINT ="fixtures?$api_token"
         const val TEAM_END_POINT ="teams?$api_token"
+       // const val FIXTURE_END_POINT ="fixtures?$api_token"
 
-        //https://cricket.sportmonks.com/api/v2.0/countries?api_token=Wy9K8UlUMHGRkfslTawlhRtVk3v47DIhh2VCgfPhfww0ox42CiJ5aECYEe7h
+        //https://cricket.sportmonks.com/api/v2.0/fixtures?filter[starts_between]=2023-01-02T00:00:00.000000Z,2023-02-10T00:00:00.000000Z
+    }
+    fun dateFormat(date: String): String{
 
+        val dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        val sdf = SimpleDateFormat(dateFormat, Locale.getDefault())
+        val inputTime = sdf.parse(date).time
+        val currentTime = System.currentTimeMillis()
+        val difference = currentTime - inputTime
+        val hoursAgo = (difference / (1000 * 60 * 60)).toInt()
+        val daysAgo = (difference / (1000 * 60 * 60*24)).toInt()
+
+        if (hoursAgo<25){
+            return ("$hoursAgo hours ago")
+        }
+        else{
+            return ("$daysAgo days ago")
+        }
     }
 }
