7c0e65c07156f0829e329a7d67ca91ebf58aefc1
Samiun Rahman Sizan <samiun.rahman@bjitacademy.com>
2023-02-15 16:58:47 +0600

    Details SHow

--- app/src/main/java/com/samiun/mycricket/ui/MainActivity.kt
+++ app/src/main/java/com/samiun/mycricket/ui/MainActivity.kt
@@ -20,7 +20,9 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.navController
         setupActionBarWithNavController(navController)
+    }
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
